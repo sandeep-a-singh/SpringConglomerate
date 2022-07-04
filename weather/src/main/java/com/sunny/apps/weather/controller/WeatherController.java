aa004e29e16d596df44724c12545656d753851f0
Sandeep Singh <sunny9969888992@gmail.com>
2022-07-04 14:00:24 +0530

    Update WeatherController.java

--- weather/src/main/java/com/sunny/apps/weather/controller/WeatherController.java
+++ weather/src/main/java/com/sunny/apps/weather/controller/WeatherController.java
@@ -1,16 +1,21 @@
 package com.sunny.apps.weather.controller;
 
-import com.sunny.apps.weather.model.WeatherResult;
-import com.sunny.apps.weather.service.WeatherService;
 import net.logstash.logback.argument.StructuredArguments;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sunny.apps.weather.model.WeatherResult;
+import com.sunny.apps.weather.service.WeatherService;
 
 @RestController
 @RequestMapping(path = "/weather")
@@ -22,7 +27,7 @@ public class WeatherController {
   WeatherService weatherService;
 
   @GetMapping("/city")
-  public ResponseEntity getWeather(@RequestParam String city) {
+  public ResponseEntity<WeatherResult> getWeather(@RequestParam String city) {
     WeatherResult weatherResult = null;
     try {
       weatherResult = weatherService.getWeather(city);
@@ -31,4 +36,13 @@ public class WeatherController {
     }
     return ResponseEntity.ok(weatherResult);
   }
+
+  @ExceptionHandler(RuntimeException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public ResponseEntity<String> handleRuntimeException(RuntimeException exception) {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                         .body(exception.getMessage());
+  }
+
 }
+
